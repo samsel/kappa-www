8b6f3fe66bfe4dd1ca4fb06b5a39b7a5a7e3894e
Sam Selvanathan <santhonyselvanat@paypal.com>
2014-07-06 08:54:07 -0700

    a simple hapi plugin to render page based on web request

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var hapi = require('hapi'),
+    pkg = require('./package'),
+    detector = require('./lib/detector');
+
+var isWebRequest = {
+    method: function(req, reply) {
+        reply(detector.isBrowser(req));
+    },
+    assign: "isWebRequest"
+};
+
+var render = function (req, reply) {
+    reply("yeah browser");
+};
+
+module.exports = {
+
+    name: pkg.name,
+
+    version: pkg.version,
+
+    register: function (plugin, options, next) {
+
+        plugin.expose({
+            isWebRequest: isWebRequest,
+            render: render
+        }); 
+
+        next();
+    }
+};
