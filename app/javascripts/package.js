921e3ae86d59af3e6bede57cf6dd571154a45577
Sam Selvanathan <santhonyselvanat@paypal.com>
2014-07-17 10:56:50 -0700

    minor ui tweak

--- app/javascripts/package.js
+++ app/javascripts/package.js
@@ -1,3 +1,3 @@
 define(['jquery', 'jsonHuman'], function ($, JsonHuman) {
-	$('#info').append(JsonHuman.format(window.packageInfo)).slideDown(1000);
+	$('#info').append(JsonHuman.format(window.packageInfo)).fadeIn(300);
 });
